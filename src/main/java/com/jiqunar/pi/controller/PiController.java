cbe183c78069b87183b9eb40e4f72c041cefee1e
wjg50058 <jieguang.wang@ly.com>
2020-09-11 23:24:49 +0800

    led

--- src/main/java/com/jiqunar/pi/controller/PiController.java
+++ src/main/java/com/jiqunar/pi/controller/PiController.java
@@ -1,5 +1,9 @@
 package com.jiqunar.pi.controller;
 
+import com.jiqunar.pi.util.PiUtil;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +19,9 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("pi")
 public class PiController {
+    final GpioController gpio = PiUtil.getGpioController();
+    final GpioPinDigitalOutput pin11 = PiUtil.getGpio11();
+
     /**
      * test
      *
@@ -23,5 +30,28 @@ public class PiController {
     @GetMapping("test")
     public LocalDateTime test() {
         return LocalDateTime.now();
+    }
+
+    /**
+     * led
+     *
+     * @return
+     */
+    @GetMapping("led")
+    public String led(Integer state) {
+        String result = "";
+        pin11.setShutdownOptions(true, PinState.LOW);
+        if (state == 0) {
+            pin11.low();
+        } else {
+            pin11.high();
+        }
+        if (pin11.isHigh()) {
+            result = "已开启";
+        }
+        if (pin11.isLow()) {
+            result = "已关闭";
+        }
+        return result;
     }
 }
