a4ed3554eb2e6030de2354eda22d8f0269f5d3a3
wjg50058 <jieguang.wang@ly.com>
2020-09-17 16:51:35 +0800

    uln

--- src/main/java/com/jiqunar/pi/controller/PiController.java
+++ src/main/java/com/jiqunar/pi/controller/PiController.java
@@ -1,6 +1,7 @@
 package com.jiqunar.pi.controller;
 
 import com.pi4j.io.gpio.*;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,12 @@ import java.time.LocalDateTime;
 @RequestMapping("pi")
 public class PiController {
     final GpioController gpio = GpioFactory.getInstance();
-    GpioPinDigitalOutput pin11 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, "MyLed", PinState.LOW);
+    final GpioPinDigitalOutput pin11 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_00, PinState.LOW);
+
+    final GpioPinDigitalOutput pin35 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, PinState.LOW);
+    final GpioPinDigitalOutput pin36 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, PinState.LOW);
+    final GpioPinDigitalOutput pin37 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, PinState.LOW);
+    final GpioPinDigitalOutput pin38 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, PinState.LOW);
 
     /**
      * test
@@ -49,5 +55,33 @@ public class PiController {
             result = "已关闭";
         }
         return result;
+    }
+
+    /**
+     * uln2003点击转动
+     *
+     * @return
+     */
+    @SneakyThrows
+    @GetMapping("uln")
+    public void led() {
+        for (int i = 0; i < 2048; i++) {
+            setPinHighLow(PinState.LOW, PinState.LOW, PinState.LOW, PinState.HIGH);
+            Thread.sleep(2);
+            setPinHighLow(PinState.LOW, PinState.LOW, PinState.HIGH, PinState.LOW);
+            Thread.sleep(2);
+            setPinHighLow(PinState.LOW, PinState.HIGH, PinState.LOW, PinState.LOW);
+            Thread.sleep(2);
+            setPinHighLow(PinState.HIGH, PinState.LOW, PinState.LOW, PinState.LOW);
+            Thread.sleep(2);
+        }
+        setPinHighLow(PinState.LOW, PinState.LOW, PinState.LOW, PinState.LOW);
+    }
+
+    private void setPinHighLow(PinState pinState35, PinState pinState36, PinState pinState37, PinState pinState38) {
+        pin35.setState(pinState35);
+        pin36.setState(pinState36);
+        pin37.setState(pinState37);
+        pin38.setState(pinState38);
     }
 }
